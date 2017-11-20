7d17a36d1cc4c1f8af306dcdf681f0251017bcda
roopa26 <roopashree65@gmail.com>
2017-11-20 14:21:17 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string unicodeText = this.textBox1.Text;
+            byte[] byteData = Encoding.UTF8.GetBytes(unicodeText);
+
+            for (int i = 0; i < byteData.Length; i++)
+            {
+                this.textBox2.Text += byteData[i].ToString();
+            }
+        }
+    }
+}
